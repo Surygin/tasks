eb1aac614010aa306e7dfdfcf14e2ef64bc31bf7
Антон Сурыгин <surygin@live.com>
2021-05-15 21:59:51 +0300

    PHP урок 5 - первая версия

--- bd.php
+++ bd.php
@@ -41,4 +41,55 @@
       ]
     ],
   ];
+
+  $lesson5 = [
+              'person1' =>  [
+                              'name'  =>  'Sunny A. (UI/UX Expert)',
+                              'role'  =>  'Lead Author',
+                              'img'   =>  [
+                                            'path'  =>  'img/demo/authors/sunny.png',
+                                            'alt'   =>  'Sunny A.',
+                              ],
+                              'contact1'      =>  'https://twitter.com/@atlantez',
+                              'contact1-title'      =>  '@myplaneticket',
+                              'contact2'      => 'https://wrapbootstrap.com/user/myorange',
+                              'contact2-title' =>  'Contact Sunny',
+              ],
+              'person2' =>  [
+                              'name'  =>  'Jos K. (ASP.NET Developer)',
+                              'role'  =>  'Partner &amp; Contributor',
+                              'img'   =>  [
+                                            'path'  =>  'img/demo/authors/josh.png',
+                                            'alt'   =>  'Jos K.',
+                            ],
+                              'contact1'       =>  'https://twitter.com/@atlantez',
+                              'contact1-title'       =>  '@atlantez',
+                              'contact2'       =>  'https://wrapbootstrap.com/user/Walapa',
+                              'contact2-title' =>  'Contact Sunny',
+              ],
+              'person3' =>  [
+                              'name'  =>  'Jovanni L. (PHP Developer)',
+                              'role'  =>  'Partner &amp; Contributor',
+                              'img'   =>  [
+                                            'path'  =>  'img/demo/authors/jovanni.png',
+                                            'alt'   =>  'Jovanni Lo',
+                            ],
+                              'contact1'       =>  'https://twitter.com/@lodev09',
+                              'contact1-title'       =>  '@lodev09',
+                              'contact2'       =>  'https://wrapbootstrap.com/user/lodev09',
+                              'contact2-title' =>  'Contact Jovanni',
+              ],
+              'person4' =>  [
+                                'name'  =>  'Roberto R. (Rails Developer)',
+                                'role'  =>  'Partner &amp; Contributor',
+                                'img'   =>  [
+                                              'path'  =>  'img/demo/authors/roberto.png',
+                                              'alt'   =>  'Jovanni Lo',
+                              ],
+                                'contact1'       =>  'https://twitter.com/@sildur',
+                                'contact1-title'       =>  '@sildur',
+                                'contact2'       =>  'https://wrapbootstrap.com/user/sildur',
+                                'contact2-title' =>  'Contact Roberto',
+              ],
+  ];
 ?>
