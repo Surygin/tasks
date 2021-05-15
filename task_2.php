a772f07bb4b5733982f76485707a0b33768b4d10
Антон Сурыгин <surygin@live.com>
2021-05-15 14:09:10 +0300

    PHP урок 2 - вторая версия

--- task_2.php
+++ task_2.php
@@ -27,6 +27,8 @@
                         $articles = [
                             'item1' => [
                                 'id'    => 1,
+                                'class' => 'mb-g',
+                                'tag'   => 'h2',
                                 'title' => 'Privacy',
                                 'text' => 'Your privacy is important to us at SmartAdmin and the protection, confidentiality and integrity of your personal data are our prime concerns.
                                             We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
@@ -35,6 +37,8 @@
                             ],
                             'item2' => [
                                 'id'    => 2,
+                                'class' => '',
+                                'tag'   => 'h3',
                                 'title' => 'Cookies and other similar technologies',
                                 'text' => 'We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.',
                             ],
@@ -54,20 +58,13 @@
                             <div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
                             
                             <?php
-                            foreach ($articles as  $article) {
-                                if ($article['id'] == 1){
+                            foreach ($articles as $article) {
+                                
                                     ?>
-                                    <h2><?php echo $article['title']?></h2>
-                                    <p class="mb-g"><?php echo $article['text']?></p>
+                                    <<?php echo $article['tag'] ?>><?php echo $article['title']?></<?php echo $article['tag'] ?>>
+                                    <p class="<?php echo $article['class'] ?>>"><?php echo $article['text']?></p>
                                     <?php
-                                    $i = $i + 1;
-                                }
-                                else {
-                                    ?>
-                                    <h3><?php echo $article['title']?></h3>
-                                    <p><?php echo $article['text']?></p>
-                                    <?php
-                                }
+                                
                             }
                             ?>
                         </div>
